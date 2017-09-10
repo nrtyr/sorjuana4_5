ffb2fd959f0f5b470e6719059c0b7e7d6e968115
nrtyr <hackuna@hackuna885.local>
2017-09-10 14:10:08 -0500

    sorjuana4_5
    Esta es una demo de lo que puede ser sorjuana

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Sor Juana Inés de la Cruz, S.C.</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/menu.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/fonts.css">
+</head>
+<body>
+<header>
+	
+	<div class="menu">
+		<div class="btnMenu"><i class="fa fa-bars" aria-hidden="true"></i>Menu</div>
+	</div>
+		<nav>
+			<div class="ok">
+			<ul>
+				<li><a href="#">Inicio<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+					<ul>
+						 <li><a href="#">¿Quienes Somos?</a></li>
+						 <li><a href="#">Mensaje del Rector</a></li>
+						 <li><a href="#">Submenu 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Preescolar</a></li>
+				<li><a href="#">Primaria</a></li>
+				<li><a href="#">Secundaria</a></li>
+				<li><a href="#">Preparatoria</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+			</div>
+		</nav>
+		
+	
+</header>
+
+	<div class="bloque paralax img0">
+		<div class="contenedor cont0">
+			<h2>International College</h2>
+			<h1>Sor Juana Inés de la Cruz, S. C.</h1>
+		</div>
+	</div>
+	<div class="bloque">
+		<div class="contenedor">	
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates neque corrupti accusantium ipsum, qui ut, dolorum incidunt cumque ullam sunt dicta labore inventore quisquam necessitatibus ipsam nostrum repudiandae voluptatem. Molestias!</p>
+
+		</div>
+	</div>
+	<div class="bloque paralax img1">
+		<div class="contenedor uno">
+			<h1>Preescolar</h1>
+		</div>
+	</div>
+	<div class="bloque">
+		<div class="contenedor cUno">	
+			<h3>MODELO EDUCATIVO</h3>
+
+			<p>Está basado en propósitos pedagógicos que favorecen de manera prioritaria el desarrollo de competencias; en las que los niños adquieren experiencias cimentadas en conductas y valores que determinarán sus relaciones futuras de manera armónica.</p>
+
+			<h3>ACTIVIDADES FORMATIVAS</h3>
+
+			<p>Diseñamos situaciones didácticas que favorecen en nuestros alumnos el desarrollo de competencias de los 6 campos formativos propuestos por la nueva Reforma Educativa de la Secretaría de Educación Pública; dichas actividades se centran en el aprendizaje significativo, que se logra por medio de la práctica de problemas reales dentro y fuera de la Institución.</p>
+
+			<h3>ACTIVIDADES ACADÉMICAS</h3>
+
+			<p>Nuestros programas contemplan actividades tanto en inglés como en español acompañados del uso de la tecnología, con la finalidad de proporcionar a los alumnos las herramientas necesarias para la adquisición de una formación integral, implementamos a nuestros pequeños la ciase de Robótica. Así mismo contamos con una infraestructura de vanguardia que fortalece nuestra tarea educativa.</p>
+
+
+			<h3>ACTIVIDADES DEPORTIVAS Y ARTÍSTICAS</h3>
+
+			<p>Promovemos actividades deportivas y artísticas con la finalidad de inculcar desde temprana edad los hábitos de disciplina, constancia y perseverancia, como reforzamiento tenemos el taller de ''Pequeños Ninjas", mediante juegos encausamos la actividad motriz desarrollando la confianza en espacio y tiempo recreativo aunado al mejoramiento de la memorización elevando el potencial académico del alumno; así como el gusto por las actividades artísticas como el ballet, música, pintura entre otras.</p>
+		</div>
+	</div>
+	<div class="bloque paralax img2">
+		<div class="contenedor dos">
+			<h1>Primaria</h1>
+		</div>
+	</div>
+	<div class="bloque">
+		<div class="contenedor">	
+			<h3>MODELO EDUCATIVO</h3>
+
+			<p>Promueve la participación activa de los alumnos y de la familia, con asignaturas centradas en el proceso de aprendizaje, evaluación formativa continua, y promoción del pensamiento crítico, reflexivo y lógico-matemático basado en un modelo ecléctico e integra elementos de los métodos activo y constructivista, favoreciendo así un desarrollo armónico de la personalidad del educando.</p>
+
+			<h3>ACTIVIDADES FORMATIVAS</h3>
+
+			<p>La educación de nuestra Institución tiene especial interés en fomentar el bienestar del alumno mejorando la calidad de sus relaciones interpersonales y su participación en la comunidad a través de convivencias, campamentos y viajes nacionales e internacionales.</p>
+
+			<h3>ACTIVIDADES ACADÉMICAS</h3>
+
+			<p>La Institución ofrece un clima favorable para el aprendizaje y el espíritu innovador, desarrollando habilidades, hábitos y destrezas, por medio de programas educativos y tecnología de vanguardia, por lo que se les asegura la calidad en sus estudios: En lo académico, lo artístico, lo cultural y lo espiritual.
+			</p>
+
+			<p>Así mismo promueve el conocimiento del idioma inglés mediante la presentación del examen Ket (Key English Test) en The University of Cambridge, CeNNI (Certificado Nacional del Nivel de Inglés) con reconocimiento a nivel Mundial, aunado al inglés se imparten también clases de Francés, Chino o Mandarín y clases de Robótica haciendo a nuestros alumnos altamente competitivos en el contexto mundial.</p>
+
+
+			<h3>ACTIVIDADES DEPORTIVAS Y ARTÍSTICAS</h3>
+
+			<p>El cuidado de la salud corporal lleva a la práctica de los deportes que estimulan la formación del carácter, para lo cual la primaria ofrece disciplinas como; el karate, danza moderna y contemporánea, jazz, ballet, hawaiano, football, basketball, también cultiva la sensibilidad artística por medio del teatro, la música y la pintura.</p>
+		</div>
+	</div>
+	<div class="bloque paralax img3">
+		<div class="contenedor tres">
+			<h1>Secundaria</h1>
+		</div>
+	</div>
+	<div class="bloque">
+		<div class="contenedor">	
+			<h3>MODELO EDUCATIVO</h3>
+
+			<p>Se caracteriza por promover el liderazgo, ser positivo, constructivo y propositivo. Interpreta el presente educativo y mira hacia el futuro, haciendo tomar conciencia a la persona y comprometiéndose con el desarrollo de su entorno, para mejorarlo en lo humano, lo científico, lo tecnológico, lo cultural, lo espiritual, lo económico y lo político.</p>
+
+			<h3>ACTIVIDADES FORMATIVAS</h3>
+
+			<p>Propiciamos que nuestros alumnos participen de manera activa en su formación humana fomentando sus relaciones interpersonales, a través de retiros y campamentos.</p>
+
+			<h3>ACTIVIDADES ACADÉMICAS</h3>
+
+			<p>Contamos con diferentes programas educativos y tecnología de vanguardia, ello asegura calidad en lo académico y lo cultural. Otra prioridad son los idiomas inglés y francés, mediante la presentación de los exámenes de certificación a través de University of Cambridge (ESOL Examinations), con reconocimiento a nivel mundial.</p>
+
+
+			<h3>ACTIVIDADES DEPORTIVAS Y ARTÍSTICAS</h3>
+
+			<p>Dentro de la educación integral de los alumnos está su formación física, por lo que se ofrecen diferentes disciplinas deportivas y artísticas ( karate, football, basketball, volleyball, hawaiano, tahitiano, grupo de jazz, bastoneras, coro, ajedrez, teatro, pintura, taller de guitarra, etc.) para su sano desarrollo, fomentando en ellos el espíritu de colaboración y triunfo.</p>
+		</div>
+	</div>
+	<div class="bloque paralax img4">
+		<div class="contenedor cuatro">
+			<h1>Preparatoria</h1>
+		</div>
+	</div>
+	<div class="bloque">
+		<div class="contenedor">	
+			<h3>MODELO EDUCATIVO</h3>
+
+			<p>Contribuir a la formación de personas alegres, respetuosas y competitivas.</p>
+
+			<p>Somos una Institución educativa que ofrece educación integral de calidad a nivel Medio Superior, con reconocimiento oficial ante la Secretaría de Educación Pública. Nuestros alumnos cuentan con un sistema de educación de vanguardia, que tiene como misión formar de manera integral líder, creativos, propositivos y responsables.</p>
+
+			<h3>ACTIVIDADES FORMATIVAS</h3>
+
+			<p>Nuestro objetivo es impartir estudios de nivel Medio Superior de tipo Bachillerato Tecnológico en modalidad escolarizada y vertiente bivalente, con la carrera de técnico en informática.</p>
+			<p>“Certificado de Nivel Bachillerato Tecnológico”</p>
+			<p>“Título de Técnico en Informática”</p>
+			<p>“Norma de Competencia Laboral“</p>
+			<p>“Experiencia laboral”</p>
+
+			<h3>ACTIVIDADES ACADÉMICAS</h3>
+
+			<p>Contamos con un importante centro educativo bilingüe, mediante una gama de exámenes Cambridge English For School en la que ofrecemos una evaluación externa e independiente de la capacidad lingüistica de cada estudiante según estándares internacionales. Aprender inglés y obtener el certificado adecuado darán a sus hijos una ventaja competitiva en la vida, la cual abrirá puertas en el mundo académico que los ¡levarán a mejores oportunidades, en México y el extranjero. Además se ha incrementado a este plan académico las asignaturas de Criminalística, Diseño Gráfico, Diseño Industrial y Gastronomía.</p>
+
+
+			<h3>ACTIVIDADES DEPORTIVAS Y ARTÍSTICAS</h3>
+
+			<p>De acuerdo a sus intereses y habilidades personales, nuestros alumnos tiene a su alcance diversas alternativas asi como modernas instalaciones adecuadas para la práctica de actividades y talleres como mecatrónica, jazz, cardio, volleyball, basketball, etc.</p>
+		</div>
+		<footer>
+		<div class="pie">
+			<p>International College</p>
+			<p class="txtGrande">Sor Juana Inés de la Cruz. S.C.</p>
+			<p>Jardín Hidalgo S/N. Col. Hidalgo, Cd. Nicolás Romero</p>
+			<p>Edo. de México, C.P. 54434. Tel: 5823-1499.</p>
+		</div>
+	</footer>
+	</div>
+	<script src="js/jquery.js"></script>
+	<script src="js/menu.js"></script>
+</body>
+</html>
